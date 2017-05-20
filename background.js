fde32f663ea1d6bd147c2bd06225247aad3e7b24
“FreMaNgo” <boping2010@gmail.com>
2017-05-20 14:22:15 +0800

    update

--- background.js
+++ background.js
@@ -1,10 +1,27 @@
+const API = 'https://api.ocr.space/parse/imageurl';
+
 chrome.contextMenus.create({
-  title: 'right menu',
+  title: 'codes',
   contexts: ['image'],
-  onclick(info, tab) {
-    let imgUrl = info.srcUrl;
-    
+  onclick: update
+}, () => {});
+
+function update(info, tab) {
+  let imgUrl = info.srcUrl;
+
+  if (imgUrl.indexOf('data') === 0) {
+    return;
   }
-}, () => {
+
+  const res = axios.get(API, {
+    params: {
+      apikey: '2feae35e4188957',
+      url: imgUrl
+    }
+  });
   console.log('successed');
-});
+
+  res.then((response) => {
+    console.log(response);
+  });
+}
