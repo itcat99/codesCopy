7a1b8a633143f4ee0f070457d9a0278a12c58609
JimbaChen <429000366@qq.com>
2017-05-20 14:59:43 +0800

    add api

--- background.js
+++ background.js
@@ -1,26 +1,26 @@
-const API = 'https://localhost:3000';
+const API = 'http://localhost:3000';
 
 chrome.contextMenus.create({
-  title: 'codes',
-  contexts: ['image'],
-  onclick: update
+    title: 'codes',
+    contexts: ['image'],
+    onclick: update
 }, () => {});
 
 function update(info, tab) {
-  let imgUrl = info.srcUrl;
+    let imgUrl = info.srcUrl;
 
-  if (imgUrl.indexOf('data') === 0) {
-    return;
-  }
-
-  const res = axios.get(API, {
-    params: {
-      url: imgUrl
+    if (imgUrl.indexOf('data') === 0) {
+        return;
     }
-  });
-  console.log('successed');
 
-  res.then((response) => {
-    console.log(response);
-  });
+    const res = axios.get(API, {
+        params: {
+            url: imgUrl
+        }
+    });
+    console.log('successed');
+
+    res.then((response) => {
+        console.log(response);
+    });
 }
