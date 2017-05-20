9f9508dc76f829471ffd5d08d2222a16920e4766
JimbaChen <429000366@qq.com>
2017-05-21 01:20:50 +0800

    change encodeURI to escape

--- background.js
+++ background.js
@@ -1,26 +1,26 @@
 const API = 'http://localhost:3001';
 
 chrome.contextMenus.create({
-  title: '扫描--发现新世界',
-  contexts: ['image'],
-  onclick: scan
+    title: '扫描--发现新世界',
+    contexts: ['image'],
+    onclick: scan
 }, () => {});
 
 function scan(info, tab) {
-  let imgUrl = info.srcUrl;
+    let imgUrl = info.srcUrl;
 
-  if (imgUrl.indexOf('data') === 0) return;
+    if (imgUrl.indexOf('data') === 0) return;
 
 
-  axios.post(API, {
-    url: imgUrl
-  }).then((res) => {
-    console.log(res.data);
-    const data = encodeURI(res.data);
-    const code = `document.getElementById("codesCopy").style.display = "block"; document.getElementById("codesCopyInput").value = decodeURI("${data}")`;
-    console.log(code);
-    chrome.tabs.executeScript({
-      code: code
+    axios.post(API, {
+        url: imgUrl
+    }).then((res) => {
+        console.log(res.data);
+        const data = escape(res.data);
+        const code = `document.getElementById("codesCopy").style.display = "block"; document.getElementById("codesCopyInput").value = unescape("${data}")`;
+        console.log(code);
+        chrome.tabs.executeScript({
+            code: code
+        });
     });
-  });
 }
