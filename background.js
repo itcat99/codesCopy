9bc682bdb7f89c7936327ba837c00dac2fd80b18
JimbaChen <429000366@qq.com>
2017-05-20 15:55:00 +0800

    add jquery

--- background.js
+++ background.js
@@ -3,24 +3,24 @@ const API = 'http://localhost:3000';
 chrome.contextMenus.create({
     title: 'codes',
     contexts: ['image'],
-    onclick: update
+    onclick: scan
 }, () => {});
 
-function update(info, tab) {
+function scan(info, tab) {
     let imgUrl = info.srcUrl;
 
-    if (imgUrl.indexOf('data') === 0) {
-        return;
-    }
+    if (imgUrl.indexOf('data') === 0) return;
 
-    const res = axios.get(API, {
+    axios.get(API, {
         params: {
             url: imgUrl
         }
-    });
-    console.log('successed');
-
-    res.then((response) => {
-        console.log(response);
+    }).then((res) => {
+        const data = encodeURI(res.data);
+        const code = `console.log("${data}")`;
+        console.log(code);
+        chrome.tabs.executeScript({
+            code
+        });
     });
 }
