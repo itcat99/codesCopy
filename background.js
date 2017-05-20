55c1124fc0793fa667425e68bcca5c23b06fad8b
FreMaNgo <boping2010@gmail.com>
2017-05-20 23:38:00 +0800

    extend by tesseract

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
 const API = 'http://localhost:3001';
+const tesseract = window.Tesseract;
+// tesseract.workerOptions.workerPath = chrome.runtime.getURL('./node_modules/tesseract.js/dist/worker.js');
+// tesseract.workerOptions.corePath = chrome.runtime.getURL('./node_modules/tesseract.js-core/index.js');
+
+console.log(tesseract);
+// console.log(chrome.runtime.getURL('images/fre.png'))
 
 chrome.contextMenus.create({
   title: '扫描--发现新世界',
@@ -11,16 +17,30 @@ function scan(info, tab) {
 
   if (imgUrl.indexOf('data') === 0) return;
 
+  tesseract.recognize(imgUrl)
+    .then((result) => {
+      console.log('successed!');
+      console.log(result);
 
-  axios.post(API, {
-    url: imgUrl
-  }).then((res) => {
-    console.log(res.data);
-    const data = encodeURI(res.data);
-    const code = `document.getElementById("codesCopy").style.display = "block"; document.getElementById("codesCopyInput").value = decodeURI("${data}")`;
-    console.log(code);
-    chrome.tabs.executeScript({
-      code: code
+      let value = result.text;
+      let data = JSON.stringify({
+        "type":"codes-copy",
+        "value": value
+      });
+      chrome.tabs.executeScript({
+        code: `window.postMessage(${data}, "*")`
+      });
     });
-  });
+
+  // axios.post(API, {
+  //   url: imgUrl
+  // }).then((res) => {
+  //   console.log(res.data);
+  //   const data = encodeURI(res.data);
+  //   const code = `document.getElementById("codesCopy").style.display = "block"; document.getElementById("codesCopyInput").value = decodeURI("${data}")`;
+  //   console.log(code);
+  //   chrome.tabs.executeScript({
+  //     code: code
+  //   });
+  // });
 }
--- content/content.js
+++ content/content.js
@@ -1,66 +1,30 @@
-var w, h, className;
+window.onload = () => {
+  console.log('window onload');
 
-$(function () {
-  let template = `<div id="codesCopy">
-
-	<header><h2>代码收藏</h2></header>
-	
-	<div class="box">
-		<div class="demo">
-			<a href="javascript:;" class="bounceIn">按钮</a>
-		</div>
-		
-		<div id="dialogBg"></div>
-		<div id="dialog" class="animated">
-			<img class="dialogIco" width="50" height="50" src="images/ico.png" alt="" />
-			<div class="dialogTop">
-				<a href="javascript:;" class="claseDialogBtn">close</a>
-			</div>
-			<form action="" method="post" id="editForm">
-				&nbsp;&nbsp;<h3>代码工具</h3>
-				<ul class="editInfos">
-				<li><textarea id="codesCopyInput" name="reworkmes" cols="60"  rows="6" style="OVERFLOW: hidden"></textarea>	</li>
-				</ul>
-			</form>
-		</div>
-	</div>
-	
-</div>
-<script>
-  function show(data) {
-    let $codesCopy = $('#codesCopy');
-    let input = $codesCopy.find('textarea');
-
-    $codesCopy.addClass('codesCopy-show');
-    $input.val(data);
-  }
-</script>
-`
+  let template = `<section id="codesCopy">
+      <h1 class="codesCopy-title"></h1>
+      <span class="codesCopy-close">X</span>
+      <div class="codesCopy-content"></div>
+    </section>
+    `
 
   $('body').append(template);
-});
+  let $wrap = $('body').find('#codesCopy');
+  let $close = $wrap.find('.codesCopy-close');
+  let $content = $wrap.find('.codesCopy-content');
 
-function getSrceenWH() {
-  w = $(window).width();
-  h = $(window).height();
-  $('#dialogBg').width(w).height(h);
-}
-
-window.onresize = function () {
-  getSrceenWH();
-}
-$(window).resize();
-
-$(function () {
-  getSrceenWH();
-
-  className = $(this).attr('class');
-  $('#dialogBg').fadeIn(300);
-  $('#dialog').removeAttr('class').addClass('animated ' + className + '').fadeIn();
-
-  $('.claseDialogBtn').click(function () {
-    $('#dialogBg').fadeOut(300, function () {
-      $('#dialog').addClass('bounceOutUp').fadeOut();
-    });
+  $close.on('click', () => {
+    $content.html('');
+    $wrap.removeClass('codesCopy-show');
   });
-});
+
+  window.addEventListener('message', (event) => {
+    if (event.data.type === 'codes-copy') {
+      let data = event.data.value;
+      console.log(data);
+      $wrap.addClass('codesCopy-show');
+
+      $content.html(data);
+    }
+  }, false);
+}
