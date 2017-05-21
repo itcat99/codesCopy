c3aed490b425dfd046caed96e6e8d5abb7343961
FreMaNgo <boping2010@gmail.com>
2017-05-21 10:23:38 +0800

    update html,css,content.js

--- background.js
+++ background.js
@@ -1,26 +1,38 @@
 const API = 'http://localhost:3001';
 
 chrome.contextMenus.create({
-    title: '扫描--发现新世界',
-    contexts: ['image'],
-    onclick: scan
+  title: '扫描--发现新世界',
+  contexts: ['image'],
+  onclick: scan
 }, () => {});
 
 function scan(info, tab) {
-    let imgUrl = info.srcUrl;
+  let imgUrl = info.srcUrl;
 
-    if (imgUrl.indexOf('data') === 0) return;
+  if (imgUrl.indexOf('data') === 0) return;
+
+  sendMessage('codesCopy-init');
+  axios.post(API, {
+    url: imgUrl
+  }).then((res) => {
+    console.log(res.data);
+    const data = escape(res.data);
+
+    sendMessage('codesCopy-codes', data);
+  });
+}
 
 
-    axios.post(API, {
-        url: imgUrl
-    }).then((res) => {
-        console.log(res.data);
-        const data = escape(res.data);
-        const code = `document.getElementById("codesCopy").style.display = "block"; document.getElementById("codesCopyInput").value = unescape("${data}")`;
-        console.log(code);
-        chrome.tabs.executeScript({
-            code: code
-        });
-    });
+/* Util Methods */
+function sendMessage(type, data = null) {
+  const value = data ? unescape(data) : '';
+
+  const code = JSON.stringify({
+    "type": type,
+    "value": value
+  });
+
+  chrome.tabs.executeScript({
+    code: `window.postMessage(${code},"*");`
+  });
 }
